28c270c171f4075621cef9ae84bb074f90b4b5be
padilhasam <jeferson.padilha86@gmail.com>
2024-12-07 11:49:00 -0300

    Deletar usuários do BD

---
+++ database/seeders/DeleteUserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DeleteUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Encontre e delete usuários específicos, por exemplo, por email
+        User::where('email', 'jeferson_death@yahoo.com.br')->delete();
+        User::where('email', 'jeffersonsantos@gmail.com.br')->delete();
+        User::where('email', 'klisgabriel@gmail.com')->delete();
+        // Adicione mais condições conforme necessário
+    }
+}
